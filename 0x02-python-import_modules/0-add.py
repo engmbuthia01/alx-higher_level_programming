c344579f44f832fc827218b1e27cd218fcf70367
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-03-20 10:56:48 -0700

    script is being directly run

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
-exec(open("add_0.py").read())
-
-result = add(a,b)
-
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
 print("{} + {} = {}".format(a, b, result))
