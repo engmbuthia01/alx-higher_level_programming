99a0ee86042c5f2106711577d339917e5d4ce2fa
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-03-20 16:37:06 -0700

    + sign operator added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     from add_0 import def add(a, b):
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a + b)))
