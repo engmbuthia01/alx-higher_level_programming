73ac62bb3fbf0b68c04a822fae81e2011aaf6e11
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-03-22 10:20:11 -0700

    README.md and lists.h files have been created. Lists.h file containts the prototype functions provided under each task

---
+++ 0x04-python-more_data_structures/lists.h
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#ifndef LISTS_H
+#define LISTS_H
+
+def square_matrix_simple(matrix=[]):
+def search_replace(my_list, search, replace):
+def uniq_add(my_list=[]):
+def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
+def number_keys(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
+def update_dictionary(a_dictionary, key, value):
+def simple_delete(a_dictionary, key=""):
+def multiply_by_2(a_dictionary):
+def best_score(a_dictionary):
+def multiply_list_map(my_list=[], number=0):
+def roman_to_int(roman_string):
+#endif
