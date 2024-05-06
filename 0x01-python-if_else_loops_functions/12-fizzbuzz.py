b3a8a839b50369fe70107a0874fca49695ea25a9
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-06 14:28:50 -0700

    initialized values

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 ==0:
             print("FizzBuzz ", end='')
-        elif num % 3:
+        elif num % 3 == 0:
             print("Fizz" , end='')
-        elif num % 5:
+        elif num % 5 == 0:
             print("Buzz" , end='')
         else:
             print(f"{num}" , end='')
