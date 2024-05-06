b6f9ebc5060af0aa72716a74806ed2617dc692a1
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-05 23:55:12 -0700

    Used a formatting specifier {:02d} to ensure numbers are printed as two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 99, 2):
-    print("{:d}".format(num), end='\n' if num == 99 else ",")
+for num in range(0, 99):
+    print("{:02d}".format(num), end='\n' if num == 99 else ",")
