5d3915b543026c18ac41190825ccc17c4ae6fac4
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-08 06:16:56 -0700

    Created a function that prints a name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module contains a function that prints a name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print the provided name.
+
+    Args:
+        first_name (str): The first name to be printed.
+        last_name (str, optional): The last name to be printed. Defaults to "".
+
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
