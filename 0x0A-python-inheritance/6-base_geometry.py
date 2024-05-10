eb3aa3e342937ca967e8d0a9e662e20490164487
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-10 02:28:06 -0700

    A public instance method with area not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module constins an empty class
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Represents BaseGeometry"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+
