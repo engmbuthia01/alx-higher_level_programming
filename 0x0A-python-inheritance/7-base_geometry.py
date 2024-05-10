5ce63ce8364058d5e5da9f64a92ce15d7385fb63
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-10 02:33:36 -0700

    Created a public instance method that validates a value

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module constins a class
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Represents BaseGeometry"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates a value as an integer
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
