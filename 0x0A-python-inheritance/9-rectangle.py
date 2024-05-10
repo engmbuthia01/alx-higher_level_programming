5b6cc7dc2429163c1524ca35030f12ad7bcfc746
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-10 04:03:09 -0700

    My bad, double underscore

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        return self._width * self._height
+        return self.__width * self.__height
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
