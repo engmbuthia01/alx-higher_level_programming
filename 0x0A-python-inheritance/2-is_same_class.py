5797f3c5856b8bee911c6427572a42d1b7b00454
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-10 01:24:26 -0700

    Replaced the else statement with Return false

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,9 +9,11 @@ def is_same_class(obj, a_class):
     """
     returns True if the object is exactly an instance of the specified class ;
     otherwise False
+    Args:
+        obj: object
+        a_class: the class
     """
 
     if type(obj) == a_class:
         return True
-    else:
-        False
+    return False
