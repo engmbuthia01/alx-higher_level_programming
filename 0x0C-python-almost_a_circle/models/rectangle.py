512f6da9858534f0631085f6e636ac99158b8ecd
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-13 03:16:12 -0700

    class Rectangle nheriting from Base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ This module defines a class rectangle
 that inherits from the base class
 """
 
-from models.base import Base:
+from models.base import Base
 
 
     class Rectangle(Base):
@@ -17,60 +17,58 @@ from models.base import Base:
         self.y = y
         super().__init__(id)
 
-    #list of getter methods used to access the attributes of a class
     @property
     def width(self):
-        """Gets the value for width"""
+        """accesses the attribute (width) of a class"""
         return self.__width
 
-    @property
-    def height(self):
-        """Gets the value for height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """Gets the value for x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """Gets the value for y"""
-        return self.__y
-
-    #list of setter methods used to modify the attributes of a class
     @width.setter
     def width(self, value):
-        """width setter"""
+        """modifies the value for width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError('width must be > 0')
+            raise ValueError("width must be > 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """accesses the attribute (height) of a class"""
+        return self.__height
+    
     @height.setter
     def height(self, value):
-        """height setter"""
+        """modifies the value for height"""
         if type(value) is not int:
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError('height must be > 0')
+            raise ValueError("height must be > 0")
         self.__height = value
+
+    @property
+    def x(self):
+        """accesses the attribute x of the class"""
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """x setter"""
+        """modifies the value for x"""
         if type(value) is not int:
-            raise TypeError('x must be an integer')
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError('x must be >= 0')
+            raise ValueError("x must be >= 0")
         self.__x = value
 
-     @y.setter
+    @property
+    def y(self):
+        """accesses the attribute Y of a class"""
+        return self.__y
+
+    @y.setter
     def y(self, value):
-        """y setter"""
-        if type(value) is not int:
-            raise TypeError('y must be an integer')
+        """modifies the value of Y"""
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError('y must be >= 0')
+            raise ValueError("y must be >= 0")
         self.__y = value
