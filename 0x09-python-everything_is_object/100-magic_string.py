2eca30498596fbc701380a2b49910c62716a8662
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-14 08:09:21 -0700

    new error

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-defines a function that returns
-a string n times
-"""
-
-
 def magic_string(times[0]):
     times[0] = times[0] + 1
     return "BestSchool" + (", BestSchool" * (times[0] - 1))
