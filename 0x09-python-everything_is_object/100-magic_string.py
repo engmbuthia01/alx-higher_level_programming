3e91ac27fb1a6e5a33495283e9d4da0d72400f9a
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-14 08:07:47 -0700

    returns a string n number of 5imes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -5,7 +5,6 @@ a string n times
 """
 
 
-def magic_string(times=[0]):
-    """returns a string n number of times"""
+def magic_string(times[0]):
     times[0] = times[0] + 1
     return "BestSchool" + (", BestSchool" * (times[0] - 1))
