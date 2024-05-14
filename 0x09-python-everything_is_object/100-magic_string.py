cc6efc1280ce2a994a288176b41f1656aa90137b
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-05-14 08:05:17 -0700

    return a string n number of times

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+defines a function that returns
+a string n times
+"""
+
+
+def magic_string(times=[0]):
+    """returns a string n number of times"""
+    times[0] = times[0] + 1
+    return "BestSchool" + (", BestSchool" * (times[0] - 1))
