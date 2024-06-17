8a36d936ae65e9b1ab2062ac31da058e90a4816c
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-06-17 18:50:27 -0400

    prints 3 lines using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,5 @@
-const messages = [
-    "C is fun",
-    "Python is cool",
-    "JavaScript is amazing"
-];
-
-for (const message of messages) {
-    console.log(message);
+#!/usr/bin/node
+const myArgs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const element of myArgs) {
+	console.log(element);
 }
