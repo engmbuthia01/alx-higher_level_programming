fd8edce4b0cf732e75068024910d91b838be2988
engmbuthia01 <kevinmbuthia354@gmail.com>
2024-07-05 08:33:13 -0700

    requests

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@ https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
+
 if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen('req') as response:
+    with urllib.request.urlopen(req) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
