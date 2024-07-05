601b965a8654edcb314a3d9dfe6b6aebf6505395
engmbuthia01 <kevinmbuthia354@gmail.com>
2024-07-05 13:39:12 -0700

    exceptions

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+takes in a URL,
+sends a request to the URL
+displays the body of the response (decoded in utf-8)
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print(e.code)
+        print(e.read())
